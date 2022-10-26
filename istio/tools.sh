8e92c8a82f389d322806ab7ea4dec1c334b3e581
Shivam Nagar <shivam.nagar@outlook.com>
2022-10-27 00:34:15 +0530

    Added istio rate limiting

---
+++ istio/tools.sh
@@ -0,0 +1,3 @@
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.15/samples/addons/grafana.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.15/samples/addons/prometheus.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.15/samples/addons/kiali.yaml
