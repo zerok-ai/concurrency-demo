dd9442222c7b910a82c7577b649b89e05e45ffd2
Avin Goyal <avingoyal01@gmail.com>
2022-10-27 21:33:50 +0530

    rate limit headers

--- xk6/script.js
+++ xk6/script.js
@@ -1,7 +1,8 @@
 import http from 'k6/http';
 import { check } from 'k6';
 import { sleep } from 'k6';
-import { Counter, Trend } from 'k6/metrics';
+import { Gauge, Counter, Trend } from 'k6/metrics';
+import { getCurrentStageIndex } from 'https://jslib.k6.io/k6-utils/1.3.0/index.js';
 
 const INITIAL_VUS = (__ENV.INITIAL_VUS) ? __ENV.INITIAL_VUS : 1000;
 const MAX_VUS = (__ENV.MAX_VUS) ? __ENV.MAX_VUS : 1000;
@@ -10,6 +11,7 @@ const RATE = __ENV.RATE;
 const STAGES = __ENV.STAGES;
 const DURATION = __ENV.DURATION;
 const TIMEUNIT = __ENV.TIMEUNIT;
+// const STAGE_TAGS = __ENV.STAGE_TAGS;
 
 const initialVUs = INITIAL_VUS;
 const maxVUs = MAX_VUS;
@@ -17,6 +19,8 @@ const duration = DURATION;
 const timeUnit = TIMEUNIT;
 
 var scenarioStage = undefined;
+var stageTags = {};
+var stageToRateLimit = {};
 
 function parseStages() {
   if (scenarioStage) {
@@ -26,12 +30,20 @@ function parseStages() {
   //1_300-2_300
   scenarioStage = [];
   var stages = STAGES.split('-');
+  var index = 0;
   stages.map(stageString => {
     var stage = stageString.split('_');
     var duration = stage[0];
-    var iterations = stage[1];
-    const transformedStage = { duration: duration, target: parseInt(iterations) };
+    var requestssString = stage[1];
+    var requests = parseInt(requestssString);
+    var iterations = requests / 4;
+    if (stage.length > 2) {
+      var rateLimit = stage[2];
+      stageToRateLimit[index + ''] = rateLimit;
+    }
+    const transformedStage = { duration: duration, target: iterations };
     scenarioStage.push(transformedStage);
+    index++;
   });
 }
 
@@ -40,6 +52,7 @@ const CHECKOUT_SCENARIO = __ENV.CHECKOUT_SCENARIO;//app-checkout, zk-checkout
 const COUPONS_SCENARIO = __ENV.COUPONS_SCENARIO;//app-coupons, zk-coupons
 
 var myTrend = {};
+var myGauge = new Gauge('stageGauge');
 const scenarioMetrics = ['waiting', 'duration']
 
 function createScenarios() {
@@ -59,6 +72,8 @@ function createScenarios() {
     myTrend[key] = myTrend[key] || {};
     myTrend[key][metric] = new Trend(`custom_${COUPONS_SCENARIO}_${metric}`);
   })
+
+
 
   // const scenarioStage = [
   //   // { duration: '1m', target: 100 },
@@ -155,8 +170,22 @@ const verticalScaleCount = {
   'checkout': 333 * 1000,
 }
 
+
+function processStageIndex() {
+  var stageIndex = getCurrentStageIndex();
+  myGauge.add(stageIndex);
+  return stageIndex;
+}
+
 export function checkout() {
-  const res = http.get('http://' + HOST + '/checkout?count=' + verticalScaleCount['checkout']);
+  const stageIndex = processStageIndex();
+  const params = {};
+  if (stageToRateLimit[stageIndex + '']) {
+    params['headers'] = {
+      'rate-limit': stageToRateLimit[stageIndex + ''] + ''
+    }
+  }
+  const res = http.get('http://' + HOST + '/checkout?count=' + verticalScaleCount['checkout'], params);
   scenarioMetrics.forEach((metric) => {
     myTrend[CHECKOUT_SCENARIO][metric].add(res.timings[metric], { tag: `${CHECKOUT_SCENARIO}_${metric}` });
   })
@@ -164,25 +193,16 @@ export function checkout() {
 }
 
 export function coupons() {
-  const res = http.get('http://' + HOST + '/coupons?count=' + verticalScaleCount['coupons']);
+  const stageIndex = processStageIndex();
+  const params = {};
+  if (stageToRateLimit[stageIndex + '']) {
+    params['headers'] = {
+      'rate-limit': stageToRateLimit[stageIndex + ''] + ''
+    }
+  }
+  const res = http.get('http://' + HOST + '/coupons?count=' + verticalScaleCount['coupons'], params);
   scenarioMetrics.forEach((metric) => {
     myTrend[COUPONS_SCENARIO][metric].add(res.timings[metric], { tag: `${COUPONS_SCENARIO}_${metric}` });
   })
   sleep(1);
 }
-
-// function prepareExecFn(identifier, scenarioName, host) {
-//   const hostname = hosts[host];
-//   const key = `${identifier}_${scenarioName}`;
-//   return () => {
-//     const res = http.get('http://' + hostname + '/load-test/' + scenarioName + '?count=' + verticalScaleCount[scenarioName]);
-//     check(res, {
-//       'verify homepage text': (r) =>
-//         r.body.includes(scenarioName),
-//     });
-//     scenarioMetrics.forEach((metric) => {
-//       myTrend[key][metric].add(res.timings[metric], { tag: `${scenarioName}_${metric}` });
-//     })
-//     sleep(1);
-//   }
-// }
