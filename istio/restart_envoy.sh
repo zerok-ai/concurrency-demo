597531ecd7b56e11835e18ce3709e64dcc15db77
Shivam Nagar <shivam.nagar@outlook.com>
2022-11-08 09:39:30 +0530

    Added restart script yo acommodate any number of pods.

--- istio/restart_envoy.sh
+++ istio/restart_envoy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-for i in {0..3}
+NAMESPACE="${1:-default}"
+COMMAND="${1:-quitquitquit}"
+
+podCount=$(kubectl -n default get pods --no-headers --show-labels=true | grep istio | wc -l | sed 's/^ *//g' | bc)
+count=$(( $podCount - 1 ))
+
+for i in $(seq 0 $count)
 do
-  pod=$(kubectl -n zerok get pods --output=jsonpath="{.items[$i].metadata.name}")
-  echo $pod
-  kubectl exec -n zerok --container istio-proxy -it $pod -- /bin/bash -c "curl -sf -XPOST http://127.0.0.1:15020/quitquitquit"
+  pod=$(kubectl -n $NAMESPACE get pods --output=jsonpath="{.items[$i].metadata.name}")
+  echo
+  echo "Restarting pod: "$pod
+  kubectl exec -n $NAMESPACE --container istio-proxy -it $pod -- /bin/bash -c "curl -sf -XPOST http://132.0.0.1:15020/quitquitquit"
 done
